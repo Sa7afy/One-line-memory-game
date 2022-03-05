337b80aa73f4571bd88cd73101292231de36006c
Yousef Abdalla <93826642+Sa7afy@users.noreply.github.com>
2022-03-05 19:48:57 +0200

    Update One-line-memory-game.py

--- One-line-memory-game.py
+++ One-line-memory-game.py
@@ -8,7 +8,7 @@ clear= lambda : os.system("cls")
 
 #start
 print("Welcome to One line memory game")
-print("By Yousef Abdalla")
+print("By Yousef Abdalla Attia")
 print('FCAI-CU')
 print()
 print('Game Description:')
@@ -19,103 +19,116 @@ print()
 # Data
 player1_score = 0
 player2_score = 0
-arr = []
-list1 = []
-list2=[]
-list3 = []
+random_char_list = []
+numbers = []
+characters=[]
+check_Win_list = []
 
 #levels
 
 def easy():
-    global list1
-    global list2
-    list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    list2 = ["{A}", "{A}", '{B}', '{B}', '{C}', '{C}', '{D}', '{D}', '{E}', '{E}']
+    global numbers
+    global characters
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    characters = ["{A}", "{A}", '{B}', '{B}', '{C}', '{C}', '{D}', '{D}', '{E}', '{E}']
+
 def medium():
-    global list1
-    global list2
-    list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-    list2 = ["{A}", "{A}", '{B}', '{B}', '{C}', '{C}', '{D}', '{D}', '{E}', '{E}', '{F}', '{F}', '{G}','{G}',"{H}",'{H}']
+    global numbers
+    global characters
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    characters = ["{A}", "{A}", '{B}', '{B}', '{C}', '{C}', '{D}', '{D}', '{E}', '{E}', '{F}', '{F}', '{G}','{G}',"{H}",'{H}']
+
 def hard():   
-    global list1
-    global list2
-    list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-    list2 = ["A", "A", 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J'] 
+    global numbers
+    global characters
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    characters = ["A", "A", 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J'] 
         
 # randomize list every time you play
 def random_list():
-    global list2
-    global arr
-    for i in range(len(list2)):  
-        i = random.choice(list2)
-        arr.append(i)
-        list2.remove(i)
+    global characters
+    global random_char_list
+    for i in range(len(characters)):  
+        i = random.choice(characters)
+        random_char_list.append(i)
+        characters.remove(i)
+
+#run 
+running = True
 
 
 def game():
+    running = True
     global player1_score
     global player2_score
-    global list1
-    global list2
-    global arr
+    global numbers
+    global characters
+    global random_char_list
 
-    #when the game will stop 
-    while (len(list3) < len(list1)):
+    #game loop
+    while running:
 
         print("p1 turn, p1_score: ", player1_score)
         print()
-        print(*list1)
+        print(*numbers)
+        
 
-    #player inputs in one line
+    #player1 inputs in one line
         a, b = map(int, input().split())
 
     #replace chosen numbers with its values
-        list1[a - 1], arr[a - 1] = arr[a - 1], list1[a - 1]
-        list1[b - 1], arr[b - 1] = arr[b - 1], list1[b - 1]
+        numbers[a - 1], random_char_list[a - 1] = random_char_list[a - 1], numbers[a - 1]
+        numbers[b - 1], random_char_list[b - 1] = random_char_list[b - 1], numbers[b - 1]
         
-        print(*list1)
+        print(*numbers)
     #clear screen    
         sleep(3)
         clear()
 
-        if (list1[a - 1] == list1[b - 1]):
-            list1[b - 1] = "*"
-            list1[a - 1] = "*"
-            list3.append(1)
-            list3.append(1)
+        if (numbers[a - 1] == numbers[b - 1]):
+            numbers[b - 1] = "*"
+            numbers[a - 1] = "*"
+            check_Win_list.append('*')
+            check_Win_list.append('*')
             player1_score += 1
         
         else:
-            list1[a - 1], arr[a - 1] = arr[a - 1], list1[a - 1]
-            list1[b - 1], arr[b - 1] = arr[b - 1], list1[b - 1]
+            numbers[a - 1], random_char_list[a - 1] = random_char_list[a - 1], numbers[a - 1]
+            numbers[b - 1], random_char_list[b - 1] = random_char_list[b - 1], numbers[b - 1]
+        if (len(check_Win_list) == len(numbers))  :
+            running = False  
+            
 
         print("p2 turn, p2_score: ", player2_score)
         print()
-        print(*list1)
-    
+        print(*numbers)
+        
+        #player inputs in one line
         a, b = map(int, input().split())
 
-        list1[a - 1], arr[a - 1] = arr[a - 1], list1[a - 1]
-        list1[b - 1], arr[b - 1] = arr[b - 1], list1[b - 1]
-        print(*list1)
+        numbers[a - 1], random_char_list[a - 1] = random_char_list[a - 1], numbers[a - 1]
+        numbers[b - 1], random_char_list[b - 1] = random_char_list[b - 1], numbers[b - 1]
+        print(*numbers)
 
         sleep(3)
         clear()
 
-        if list1[a - 1] == list1[b - 1]:
-            list1[b - 1] = "*"
-            list1[a - 1] = "*"
+        if numbers[a - 1] == numbers[b - 1]:
+            numbers[b - 1] = "*"
+            numbers[a - 1] = "*"
 
-            list3.append(1)
-            list3.append(1)
+            check_Win_list.append('*')
+            check_Win_list.append("*")
             
             #increase player if he gets it right 
             player2_score += 1
         
         #return replacement
         else:
-            list1[a - 1], arr[a - 1] = arr[a - 1], list1[a - 1]
-            list1[b - 1], arr[b - 1] = arr[b - 1], list1[b - 1]
+            numbers[a - 1], random_char_list[a - 1] = random_char_list[a - 1], numbers[a - 1]
+            numbers[b - 1], random_char_list[b - 1] = random_char_list[b - 1], numbers[b - 1]
+        if (len(check_Win_list) == len(numbers))  :
+            running = False      
 
 #End game situations
 def winner():
@@ -128,27 +141,32 @@ def winner():
     if player1_score == player2_score:
         print('Draw')
 
-sleep(5)
+sleep(4)
 clear()
 
 #ask for start
 print("start?")
 print("answer with YES or NO")
+print('1 for YES, 2 for NO ')
 print()
-start=input()
+start=int(input())
 
 
 print("choose level")
-print("answer with easy, medium, hard:")
+print("answer with Easy, Medium or Hard:")
+print('1 for Easy')
+print('2 for Medium')
+print('3 for Hard')
+
 print()
-Level=input()
+Level=int(input())
 
 sleep(1)
 clear()
 
 #run the game
-if start=="YES":
-    if Level=='easy':
+if start== 1:
+    if Level == 1:
         easy()
         random_list()
         game()
@@ -161,12 +179,12 @@ if start=="YES":
 #End
 
 #run the game
-if start=="YES":
-    if Level=='medium':
+if start==1:
+    if Level == 2:
         medium()
         random_list()
         game()
-        sleep(2)
+        sleep(1)
         clear()
         winner()
         print('GG everybody')
@@ -175,12 +193,12 @@ if start=="YES":
 #End
 
 #run the game
-if start=="YES":
-    if Level=='hard':
+if start == 1:
+    if Level == 3:
         hard()
         random_list()
         game()
-        sleep(2)
+        sleep(1)
         clear()
         winner()
         print()
@@ -191,6 +209,9 @@ if start=="YES":
 
 
 
-if start=="NO":
+if start == 2:
     print("Thanks for trying our game")
 #End    
+
+
+
